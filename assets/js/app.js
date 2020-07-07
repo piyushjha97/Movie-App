cfd24d90f01a41e2369d285220ad746c643bd0ca
Piyush Jha <jhapiyush2013@gmail.com>
2020-07-08 00:46:09 +0530

    HTML elements added and poster image passed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,48 @@
-searchButton
 //urls
 
 const API_KEY = 'cf3b39752a371a951346340b2f76c815';
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=cf3b39752a371a951346340b2f76c815'
-
+const imgUrl = 'https://image.tmdb.org/t/p/w500';
 const inputElement = document.querySelector("#inputValue");
 const buttonElement = document.querySelector("#searchButton");
+const movieSearchable = document.querySelector("#movie-searchable");
+
+
+
+
+
+// const renderSearchMovies(movies){
+
+
+// }
+
+function movieSelection(movies) {
+
+    return movies.map((movie) => {
+        if(movie.poster_path)
+             return `<img src = ${imgUrl + movie.poster_path} data-movie-id = {movie.id} />`;
+    })
+}
+
+ function createMovieContainer(movies) {
+
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class','movie');
+
+    const movieTemplate = `
+    <section class = "section">
+     ${movieSelection(movies)};
+     </section>`
+
+    movieElement.innerHTML = movieTemplate;
+
+    return movieElement;
+
+}
 
 buttonElement.onclick = function(event) {
-    event.preventDefault();
-
+    
+    
     const value = inputElement.value;
 
     const newUrl = url + '&query=' + value;
@@ -17,7 +50,12 @@ buttonElement.onclick = function(event) {
     fetch(newUrl) 
         .then( (res) => res.json())
         .then( (data) => {
-            console.log('data: ',data);
+             const movies = data.results;
+            const movieBlock = createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock);
+
+            console.log(movies);
+
 
         })
         .catch((err) => {
